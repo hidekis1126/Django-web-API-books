0b0fe95a14070034869b254641128bbe375a099b
hidekis1126 <consulart.hmk@gmail.com>
2022-02-15 04:03:10 +0900

    webAPI検索機能実装

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .forms import SearchForm
+import json
+import requests
 
+SEARCH_URL = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&applicationId=1077835260995827175'
+
+def get_api_data(params):
+    api = requests.get(SEARCH_URL, params=params).text
+    result = json.loads(api)
+    items = result['Items']
+    return items
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -10,3 +19,36 @@ class IndexView(View):
         return render(request, 'app/index.html', {
             'form': form,
         })
+
+    def post(self, request, *args, **kwargs):
+        form = SearchForm(request.POST or None)
+
+        if form.is_valid():
+            keyword = form.cleaned_data['title']
+            params = {
+                'title': keyword,
+                'hits': 28,
+            }
+            items = get_api_data(params)
+            book_data = []
+            for i in items:
+                item = i['Item']
+                title = item['title']
+                image = item['largeImageUrl']
+                isbn = item['isbn']
+                query = {
+                    'title': title,
+                    'image': image,
+                    'isbn': isbn,
+                }
+                book_data.append(query)
+
+            return render(request, 'app/book.html', {
+                'book_data': book_data,
+                'keyword': keyword
+            })
+
+        return render(request, 'app/index.html', {
+            'form': form
+        })
+           
